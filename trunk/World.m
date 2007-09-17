878e323a068e125642cfa938406ec71eeb430f42
gabe.schine <gabe.schine@34ec3c9a-313a-0410-badd-376414d00a92>
2007-09-17 11:32:49 +0000

    made upside down mode way better - got accelerometer working a little better

--- trunk/World.m
+++ trunk/World.m
@@ -6,6 +6,7 @@
 #define MAX_ANTS 8
 
 World *_world;
+void initialize(int);
 
 @implementation World
 
@@ -28,6 +29,9 @@ World *_world;
     accelX = accelY = 0.5f; // neutral
     accelZ = 0.0f;
 
+    noAccelCountM = 0;
+    gotAccelM = NO;
+
     urandomM = fopen("/dev/urandom", "r");
 
     return self;
@@ -38,6 +42,8 @@ World *_world;
     accelX = x;
     accelY = y;
     accelZ = z;
+    gotAccelM = YES;
+    //NSLog(@"accel: %f %f %f", x, y, z);
 }
 
 - (float) accelX { return accelX; }
@@ -111,6 +117,17 @@ World *_world;
 
 - (id) tick: (NSTimer *)timer
 {
+    if (!gotAccelM) {
+        noAccelCountM++;
+        if (noAccelCountM > 6) {
+            initialize(10);
+            noAccelCountM = 0;
+        }
+    } else {
+        noAccelCountM = 0;
+        gotAccelM = NO;
+    }
+
     // calculate dt
     NSDate *now = [[NSDate alloc] init];
 
@@ -202,3 +219,65 @@ World *_world;
 }
 
 @end
+
+#include <IOKit/IOKitLib.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+typedef struct {} *IOHIDEventSystemRef;
+typedef struct {} *IOHIDEventRef;
+float IOHIDEventGetFloatValue(IOHIDEventRef ref, int param);
+
+void handleHIDEvent(int a, int b, int c, IOHIDEventRef ptr) {
+  int type = IOHIDEventGetType(ptr);
+
+  if (type == 12) {
+    float x,y,z;
+
+    x = IOHIDEventGetFloatValue(ptr, 0xc0000);
+    y = IOHIDEventGetFloatValue(ptr, 0xc0001);
+    z = IOHIDEventGetFloatValue(ptr, 0xc0002);
+
+    // update the world's accel view
+
+    [[World singleton] updateAccelerometerInX: x Y: y Z: z];
+  }
+}
+
+
+#define expect(x) if(!x) { printf("failed: %s\n", #x);  return; }
+
+
+
+void initialize(int hz) {
+  mach_port_t master;
+  expect(0 == IOMasterPort(MACH_PORT_NULL, &master));
+  int page = 0xff00, usage = 3;
+
+  CFNumberRef nums[2];
+  CFStringRef keys[2];
+  keys[0] = CFStringCreateWithCString(0, "PrimaryUsagePage", 0);
+  keys[1] = CFStringCreateWithCString(0, "PrimaryUsage", 0);
+  nums[0] = CFNumberCreate(0, kCFNumberSInt32Type, &page);
+  nums[1] = CFNumberCreate(0, kCFNumberSInt32Type, &usage);
+  CFDictionaryRef dict = CFDictionaryCreate(0, (const void**)keys, (const void**)nums, 2, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+  expect(dict);
+
+  IOHIDEventSystemRef sys = (IOHIDEventSystemRef) IOHIDEventSystemCreate(0);
+  expect(sys);
+
+  CFArrayRef srvs = (CFArrayRef)IOHIDEventSystemCopyMatchingServices(sys, dict, 0, 0, 0);
+  expect(CFArrayGetCount(srvs)==1);
+
+  io_registry_entry_t serv = (io_registry_entry_t)CFArrayGetValueAtIndex(srvs, 0);
+  expect(serv);
+
+  CFStringRef cs = CFStringCreateWithCString(0, "ReportInterval", 0);
+  int rv = 1000000/hz;
+  CFNumberRef cn = CFNumberCreate(0, kCFNumberSInt32Type, &rv);
+
+  int res = IOHIDServiceSetProperty(serv, cs, cn);
+  expect(res == 1);
+
+  res = IOHIDEventSystemOpen(sys, handleHIDEvent, 0, 0);
+  expect(res != 0);
+}
