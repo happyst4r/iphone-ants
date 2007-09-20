295a015ec1d64dfd550048df9626057e44e5220e
gabe.schine <gabe.schine@34ec3c9a-313a-0410-badd-376414d00a92>
2007-09-20 09:37:36 +0000

    added bugs.plist and revamped everything

--- Ant.h
+++ Ant.h
@@ -3,27 +3,38 @@
 
 #import "World.h"
 #import "Behavior.h"
-
-#define MAX_ACCEL 500
+#import "Sprites.h"
 
 @interface Ant : UIWindow <Agent>
 {
     UIImageView  * viewM;
+    Sprites * spritesM;
     CGPoint posM;
     CGPoint velM;
     CGPoint fallingVelM;
     World * worldM;
+    NSDictionary *descriptionM;
     NSObject <Behavior> *behaviorM;
     float travelCounterM;
+    float widthM;
+    float heightM;
+    float scaleM;
     int currentSpriteM;
     int stateM;
     float deathCounterM;
+
     float maxVelocityM;
+    float maxAccelerationM;
+
+    float velocityVarianceFloatM;
+    float accelerationVarianceFloatM;
 }
 
-- (id) initWithPosition: (CGPoint)p velocity: (CGPoint)v world: (World *) w;
+- (id) initWithPosition: (CGPoint)p velocity: (CGPoint)v description: (NSDictionary *)desc world: (World *) w;
 - (id) moveByX: (float)x Y: (float) y;
 - (id) reposition;
 - (float) getRotation;
+- (id) updateSprite;
+- (id) updateLimitsForState;
 
 @end
